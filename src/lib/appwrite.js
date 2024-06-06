4943dc112202b59b006ceb63a2ec4eef5528154f
Harshdeep Kanhai <harshdeep@techstax.ml>
2024-06-06 16:48:33 +0530

    console log project id

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -5,7 +5,6 @@ export const PROJECT_ID = import.meta.env.VITE_APPWITE_PROJECT_ID;
 const client = new Client()
     .setEndpoint(API_ENDPOINT) 
     .setProject(PROJECT_ID);    
-
 export const account = new Account(client);
 
-// export default client;
+export default client;
